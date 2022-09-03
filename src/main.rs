50d78aae4f4c7ff8d4df0524c447eeb36ce8ae56
Stephan Seitz <stephan.seitz@fau.de>
2022-09-03 03:12:46 +0200

    Print ONNX info

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod onnx {
 }
 
 use std::{
+    collections::HashSet,
     fs::File,
     io::{BufReader, Read},
     path::PathBuf,
@@ -17,6 +18,7 @@ struct Args {
     #[clap(subcommand)]
     action: Action,
     /// ONNX file to process
+    #[clap(long, short, value_parser)]
     onnx: PathBuf,
 }
 
@@ -41,7 +43,29 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn print_info(model: onnx::ModelProto) -> anyhow::Result<()> {
-
+    println!("doc_string {:?}:", model.doc_string);
     println!("ir_version {:?}:", model.ir_version);
+    if let Some(graph) = model.graph {
+        println!("graph.doc_string {:?}:", model.doc_string);
+        for (i, node) in graph.node.iter().enumerate() {
+            println!("node {i}: {:?}:", node.name);
+        }
+        let initialized: HashSet<String> = graph
+            .initializer
+            .iter()
+            .cloned()
+            .map(|i| i.name)
+            //.chain(graph.sparse_initializer.iter().map(|i| i.name))
+            .collect();
+
+        for (i, input) in graph.input.iter().enumerate() {
+            if !initialized.contains(&input.name) {
+                println!("input {i}: {:?} {:?}:", input.name, input.r#type);
+            }
+        }
+        for (i, ouput) in graph.output.iter().enumerate() {
+            println!("output {i}: {:?}:", ouput.name);
+        }
+    }
     Ok(())
 }
